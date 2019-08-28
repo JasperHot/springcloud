1cc4e22ca24e2f822a8ddf75b5d8eae834e3999f
Jasper <2002shuqian@gmail.com>
2019-08-28 21:27:13 +0800

    packaging as war

--- eureka/src/main/java/com/example/eureka/EurekaserverApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaserverApplication.java
@@ -1,14 +1,21 @@
 package com.example.eureka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableEurekaServer
 @SpringBootApplication
+@Slf4j
 public class EurekaserverApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaserverApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(EurekaserverApplication.class, args);
+        int length = context.getBeanDefinitionNames().length;
+        log.info("Spring boot启动初始化了 {} 个 Bean", length);
     }
+
 }
