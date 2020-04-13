31fdab3b81fd93b541f491ea7ae2d60df7b319a0
Jasper <2002shuqian@gmail.com>
2020-04-13 14:50:24 +0800

    exclude DruidDataSourceAutoConfigure

--- h2database/src/main/java/com/example/h2database/SpringBootH2Application.java
+++ h2database/src/main/java/com/example/h2database/SpringBootH2Application.java
@@ -1,11 +1,12 @@
 package com.example.h2database;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
 @EnableJpaAuditing
+@SpringBootApplication(exclude={DruidDataSourceAutoConfigure.class})
 public class SpringBootH2Application {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootH2Application.class, args);
